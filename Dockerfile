e40e39cb060d2063311a51fbfbb5134760cfc53e
Philipp Böschen <gitlab@phil.deichindianer.de>
2022-03-13 16:37:06 +0100

    Rework all of it

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,6 @@ COPY go.mod go.sum ./
 RUN go mod download && go mod verify
 
 COPY . .
-RUN go build -v -o /usr/local/bin/app ./...
+RUN go build -v -o /usr/local/bin/app ./cmd/gitlab-honeycomb-buildevents/
 
 CMD ["app"]
---
+++ cmd/gitlab-honeycomb-buildevents/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/zoidbergwill/gitlab-honeycomb-buildevents-webhooks-sink/internal/hook"
+
+	"github.com/honeycombio/libhoney-go"
+	"github.com/spf13/cobra"
+)
+
+// Version is the default value that should be overridden in the
+// build/release process.
+var Version = "dev"
+
+func commandRoot(cfg *libhoney.Config) *cobra.Command {
+	root := &cobra.Command{
+		Version: Version,
+		Use:     "buildevents",
+		Short:   "buildevents creates events for your CI builds",
+		Long: `
+The buildevents executable creates Honeycomb events and tracing information
+about your Continuous Integration builds.`,
+	}
+
+	root.PersistentFlags().StringVarP(&cfg.APIKey, "apikey", "k", "", "[env.BUILDEVENT_APIKEY] the Honeycomb authentication token")
+	if apikey, ok := os.LookupEnv("BUILDEVENT_APIKEY"); ok {
+		// https://github.com/spf13/viper/issues/461#issuecomment-366831834
+		err := root.PersistentFlags().Lookup("apikey").Value.Set(apikey)
+		if err != nil {
+			log.Fatalf("failed to configure `apikey`: %s", err)
+		}
+	}
+
+	root.PersistentFlags().StringVarP(&cfg.Dataset, "dataset", "d", "buildevents", "[env.BUILDEVENT_DATASET] the name of the Honeycomb dataset to which to send these events")
+	if dataset, ok := os.LookupEnv("BUILDEVENT_DATASET"); ok {
+		err := root.PersistentFlags().Lookup("dataset").Value.Set(dataset)
+		if err != nil {
+			log.Fatalf("failed to configure `dataset`: %s", err)
+		}
+	}
+
+	root.PersistentFlags().StringVarP(&cfg.APIHost, "apihost", "a", "https://api.honeycomb.io", "[env.BUILDEVENT_APIHOST] the hostname for the Honeycomb API server to which to send this event")
+	if apihost, ok := os.LookupEnv("BUILDEVENT_APIHOST"); ok {
+		err := root.PersistentFlags().Lookup("apihost").Value.Set(apihost)
+		if err != nil {
+			log.Fatalf("failed to configure `apihost`: %s", err)
+		}
+	}
+
+	return root
+}
+
+func main() {
+	defer libhoney.Close()
+	var config libhoney.Config
+
+	root := commandRoot(&config)
+
+	// Do the work
+	if err := root.Execute(); err != nil {
+		libhoney.Close()
+		os.Exit(1)
+	}
+
+	log.SetOutput(os.Stdout)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	l, err := hook.New(hook.Config{
+		Version:         Version,
+		ListenAddr:      ":" + port,
+		HookSecret:      "",
+		HoneycombConfig: &config,
+	})
+	if err != nil {
+		log.Fatalf("failed to setup hook listener: %s", err)
+	}
+
+	log.Printf("Starting server on http://%s\n", l.HTTPServer.Addr)
+	log.Fatal(l.ListenAndServe())
+}
